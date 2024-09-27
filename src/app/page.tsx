1f2e86206945acd1302378d8f1fd48b22e7ef002
Teja Akella <takella6315@gmail.com>
2024-09-27 18:53:51 -0400

    added next.js project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 
 export default function Home() {
@@ -20,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="bg-white text-[200px]">Save and see your changes instantly hiiii.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
